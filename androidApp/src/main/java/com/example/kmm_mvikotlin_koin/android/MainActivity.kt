9eded8d693961c1fb0de64fdefd2bc662f635c3c
Farouk-abichou <117727520+Farouk-abichou@users.noreply.github.com>
2023-02-22 15:04:05 +0100

    Still No idea what im doing

--- androidApp/src/main/java/com/example/kmm_mvikotlin_koin/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/kmm_mvikotlin_koin/android/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.kmm_mvikotlin_koin.android
 import CalculatorStore
 import CalculatorStoreFactory
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Row
@@ -12,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kmm_mvikotlin_koin.Greeting
+import kotlinx.coroutines.coroutineScope
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,14 +24,15 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Row() {
-                        Button(text = "+"){
-                            CalculatorStore.Intent.Increment
-                        }
-                        Number(text = "${CalculatorStore.State()}")
-                        Button(text = "-"){
-                            CalculatorStore.Intent.Decrement
 
+                    Row() {
+                        TextButton(onClick = { Log.d("hello","${CalculatorStore.State().value}") }) {
+                            Text(text = "+")
+                        }
+
+                        Number(text = "${CalculatorStore.State()}")
+                        TextButton(onClick = { CalculatorStore.Intent.Increment }) {
+                            Text(text = "-")
                         }
                     }
                 }
@@ -38,16 +41,12 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Button(
-    text: String,
-    onClick:() -> Unit
-) {
-    Button(text = text){
-        onClick
+
+suspend fun changeValue (){
+    coroutineScope {
+        CalculatorStore.Intent.Increment
     }
 }
-
 @Composable
 fun Number(text: String) {
     Text(text = text)
